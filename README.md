e0a0c7b3d9a34581ec8d1607bbf786b3f8d274c3
Dennis Rowlader <dennis.rowlader@amway.com>
2024-11-25 19:44:50 -0500

    Started adding express.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # :rotating_light: WIP :rotating_light:
 # docx-templating-nodejs
-A node application capable of processing Microsoft Word document templates using the docxtemplater library. It can insert images and HTML formatted text into the Word template and output the resulting document as either a .docx or an identical PDF.
+An Express.js application capable of processing Microsoft Word document templates using the docxtemplater library. It can insert images and HTML formatted text into the Word template and output the resulting document as either a .docx or an identical PDF.
 ## Scope
 ### Word Document Processing
 - Accept a .docx file as input.
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
+const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/generate', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  res.send(
+    '<html><head><title>Generate Document</title></head><body><h1>Document Input</h1><form action="/generate" method="POST"><input type="text" name="firstname" /><button type="submit">Generate Doc</button></form></body></html>'
+  );
+});
+
+app.listen(3000);
